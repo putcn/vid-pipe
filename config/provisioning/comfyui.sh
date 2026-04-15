0bbe1b98c944f97ee5b02c72b22b003c2dbd21af
Xi(Darcy) Chen <putcncx@gmail.com>
2026-04-14 17:32:45 -0700

    fix: replace PRE_START_SCRIPT with proper ai-dock provisioning script for gguf install

---
+++ config/provisioning/comfyui.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# ai-dock provisioning script for vid-pipe ComfyUI container
+# This file will be sourced in init.sh
+# Docs: https://github.com/ai-dock/comfyui
+
+APT_PACKAGES=(
+    #"package-1"
+)
+
+PIP_PACKAGES=(
+    "gguf"
+)
+
+NODES=(
+    "https://github.com/city96/ComfyUI-GGUF"
+)
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
+
+function provisioning_start() {
+    if [[ ! -d /opt/environments/python ]]; then
+        export MAMBA_BASE=true
+    fi
+    source /opt/ai-dock/etc/environment.sh
+    source /opt/ai-dock/bin/venv-set.sh comfyui
+
+    provisioning_print_header
+    provisioning_get_apt_packages
+    provisioning_get_nodes
+    provisioning_get_pip_packages
+    provisioning_print_end
+}
+
+function pip_install() {
+    if [[ -z $MAMBA_BASE ]]; then
+        "$COMFYUI_VENV_PIP" install --no-cache-dir "$@"
+    else
+        micromamba run -n comfyui pip install --no-cache-dir "$@"
+    fi
+}
+
+function provisioning_get_apt_packages() {
+    if [[ -n $APT_PACKAGES ]]; then
+        sudo $APT_INSTALL ${APT_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_pip_packages() {
+    if [[ -n $PIP_PACKAGES ]]; then
+        pip_install ${PIP_PACKAGES[@]}
+    fi
+}
+
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="/opt/ComfyUI/custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                    pip_install -r "$requirements"
+                fi
+            fi
+        else
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+            if [[ -e $requirements ]]; then
+                pip_install -r "${requirements}"
+            fi
+        fi
+    done
+}
+
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n##############################################\n\n"
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete: ComfyUI will start now\n\n"
+}
+
+provisioning_start
